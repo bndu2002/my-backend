548eaf0f3a3a0aafddd170eba8e796386e887ffd
bndu2002 <bndusharma2002@gmail.com>
2022-08-08 13:33:33 +0530

    modifications

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const abc = require('../introduction/intro');
 const van =require('../logger/logger.js');
 const sha = require('../util/helper.js');
 const bnd = require('../validator/formatter.js')
+const lodashCall = require('lodash')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
@@ -12,6 +13,24 @@ router.get('/test-me', function (req, res) {
     console.log(bnd.ran.trim())
     console.log(bnd.ran.toUpperCase())
     console.log(bnd.ran.toLowerCase())
+    const arr = ["jan","feb","mar","apr","may","june","july","aug","sep","oct","nov","dec"]
+    const call = lodashCall.chunk(arr,3) 
+    console.log(call)
+    const num = [1,3,5,7,9,11,13,15,17,19]
+    const numbers = lodashCall.tail(num)
+    console.log(numbers)
+    const one =[1,2,2]
+    const two = [3,4,5]
+    const three =[3,7,8]
+    const four = [9,10,11]
+    const five =[10,6,0]
+    
+    const merge = lodashCall.union(one , two ,three ,four , five)
+    console.log(merge)
+    const create = [["save","soil"],["cauvery","calling"],["green","hands"]]
+    const createPairs = lodashCall.fromPairs(create)
+    console.log(createPairs)
+
         abc.printName() 
 
     res.send('My second ever api!')
