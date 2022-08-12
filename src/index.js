730f2e37c007c2c808b7f35a870280fe05630694
bndu2002 <bndusharma2002@gmail.com>
2022-08-12 20:31:41 +0530

    refresh

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
+mongoose.connect("mongodb+srv://vandana:7CJBNDDwPorDTTrX@cluster0.crrs6th.mongodb.net/vandana-db"
 , {
    useNewUrlParser: true 
 }
