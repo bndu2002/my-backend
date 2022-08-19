a27064279ecd2339f21e5d3ff549c6806705ddb7
bndu2002 <bndusharma2002@gmail.com>
2022-08-19 16:24:56 +0530

    edit

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -11,5 +11,12 @@ const getAuthorsData= async function (req, res) {
     res.send({data: authors})
 }
 
+const createNewAuthor = async function(req,res){
+    let author = req.body
+    let NewAuthor = await AuthorModel.create(author)
+    res.send({msg : NewAuthor})
+}
+
 module.exports.createAuthor= createAuthor
 module.exports.getAuthorsData= getAuthorsData
+module.exports.createNewAuthor= createNewAuthor
